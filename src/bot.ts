4e0eaba921add7b519ceede8367baba9f5c76acf
ikiovi <52862362+ikiovi@users.noreply.github.com>
2023-08-04 15:02:34 +0300

    Add logger

--- src/bot.ts
+++ src/bot.ts
@@ -3,21 +3,26 @@ import { Bot } from 'grammy';
 import { MyContext } from './types/context';
 import { youtubeHandler } from './handlers/youtube';
 import { limit } from '@grammyjs/ratelimiter';
+import { Roarr as log } from 'roarr';
+import { serializeError } from 'serialize-error';
 
 const token = process.env.TOKEN;
 if (!token) throw new Error('TOKEN must be provided!');
 
 const bot = new Bot<MyContext>(token);
-console.log('Started!');
+log.debug('Started!');
 
 bot.use(limit({ limit: 2 }));
 bot.command('ping', ctx => ctx.reply('pong')); // To check if bot is alive ¯\_(ツ)_/¯
 
 //#region Register handlers
-bot.on('inline_query').fork(youtubeHandler);
+bot.use(youtubeHandler);
 //#endregion
 
-bot.catch(({ name, message }) => console.error(`\x1b[97m[${new Date().toLocaleString('uk')}] \x1b[41m${name}\x1b[0m | ${message}`));
-bot.start({ drop_pending_updates: true, allowed_updates: ['inline_query', 'message', 'callback_query', 'chosen_inline_result'] });
+bot.catch(err => log.error(serializeError(err.error), 'Something went wrong...'));
+bot.start({
+    drop_pending_updates: true,
+    allowed_updates: ['inline_query', 'message', 'callback_query', 'chosen_inline_result']
+});
 
 process.once('SIGINT', bot.stop);
--- src/types/composer.ts
+++ src/types/composer.ts
@@ -4,6 +4,9 @@ import { EventEmitter } from 'events';
 import { AppEvent, AppMessage } from './messages';
 import { getUnavalableResult } from '../utils/parsing';
 import { BotError, Composer, Context, GrammyError, Middleware, NextFunction } from 'grammy';
+import { Roarr as logger } from 'roarr';
+import { serializeError } from 'serialize-error';
+import { JsonObject, Logger } from 'roarr/dist/types';
 
 const emitter = new EventEmitter();
 const emit = (msg: AppMessage) => process.send?.(msg);
@@ -12,13 +15,23 @@ process.on('message', (msg: AppMessage) => emitter.emit(`${msg.type}:${msg.id}`,
 
 export class SwitchComposer<C extends Context = Context> extends Composer<C>{
     private _isClosed = false;
+    private _composer: Composer<C> = new Composer<C>();
     private readonly id: string;
+    private log: Logger<JsonObject>;
 
     constructor(file: string, ...middleware: Array<Middleware<C>>) {
         super(...middleware);
         if (!file) throw new Error('A');
         this.id = createHash('md5').update(parse(file).name).digest('hex');
+        this.log = logger.child({ id: this.id, path: file });
         on('OPEN', this.id, this.open.bind(this));
+        this.init();
+    }
+
+    private init() {
+        this._composer.filter(() => this._isClosed)
+            .on('inline_query', async ctx => await ctx.answerInlineQuery([getUnavalableResult(this.id)], { cache_time: 0 }));
+        this._composer.errorBoundary(this.handleError.bind(this)).lazy(() => super.middleware());
     }
 
     open() {
@@ -30,17 +43,14 @@ export class SwitchComposer<C extends Context = Context> extends Composer<C>{
     }
 
     override middleware() {
-        const c = new Composer<C>();
-        c.filter(() => this._isClosed)
-            .on('inline_query', async ctx => await ctx.answerInlineQuery([getUnavalableResult(this.id)], { cache_time: 0 }));
-        c.errorBoundary(this.handleError.bind(this), super.middleware());
-        return c.middleware();
+        return this._composer.middleware();
     }
 
     private async handleError(err: BotError, next: NextFunction) {
-        if (err?.error instanceof GrammyError) return; // if query is too old
+        if (err?.error instanceof GrammyError) return;
         this.close();
-        console.error(`\x1b[97m[${new Date().toLocaleString('uk')}] \x1b[43m${err?.error?.constructor.name ?? err.name}\x1b[0m | ${err.message}`);
+        this.log.error(serializeError(err.error), 'Closing handler...');
+
         emit({ id: this.id, type: 'ERR', value: err });
         await next();
     }

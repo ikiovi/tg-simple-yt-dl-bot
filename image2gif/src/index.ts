567ada1ceec0c47ebafd8407d83ad5f601dd871c
ikiovi <52862362+ikiovi@users.noreply.github.com>
2024-01-17 19:52:12 +0300

    Add telegram to CORS

--- image2gif/src/index.ts
+++ image2gif/src/index.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import { spawn } from 'child_process';
 
 const server = fastify();
+server.register(cors, {
+    origin: /^(.*\.)?telegram\.org$/g
+});
 
 server.get('/2gif', (req, res) => {
     const query = req.query as Record<string, string>;
@@ -19,7 +23,7 @@ server.listen({ port: +(process.env.PORT ?? 8080), host: process.env.HOST ?? '12
     console.log(`Server listening at ${address}`);
 });
 
-function convertToGif(url: string, duration = 1) {
+function convertToGif(url: string, duration = 4) {
     const ffmpeg = spawn(process.env.FFMPEG_PATH!, [
         '-hide_banner',
         '-i', url,
